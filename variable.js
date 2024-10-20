91031bc0a1d18759028df5e497eecd5ac142c83f
AshwiniGaikwad3819 <ashwinigaikwad3819@gmail.com>
2024-10-21 01:16:40 +0530

    variables

---
+++ variable.js
@@ -0,0 +1,16 @@
+!DOCTYPE ;
+<html>
+    <body>
+        <h1>JvaScript variables</h1>
+        <p>in this example,x,y,and z are undeclared.</p>
+        <p id ="demo"></p>
+        <script>
+            x=5;
+            y=6;
+            z= x+y;
+            document .grtElementById("demo").innerHTML="The value of z is:" + z;
+            </script>
+
+    </body>
+
+</html>
