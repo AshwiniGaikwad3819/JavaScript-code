a5aec1e13a4c77657b4f9de9f6f4e3f748d72210
AshwiniGaikwad3819 <ashwinigaikwad3819@gmail.com>
2024-10-22 15:41:14 +0530

    Statement

---
+++ Ternary oper.js
@@ -0,0 +1,5 @@
+let age=25;
+let = (age >=25) ? "Adult" :"Minor";
+console.log(result);
+
+
---
+++ if  else .js
@@ -0,0 +1,35 @@
+/*let age=20;
+if(age>=20){
+console.log("your an Adult");
+}else{
+    console.log("you  are a minor");
+}*/
+
+
+//2)if -else 
+
+//if-else exampal
+/*let score="85";
+if(score>=90){
+console.log( "GradeSS A");
+} else if(score>=80){
+        console.log ("Gread B");
+
+        }else{
+            console.log("Gread c")
+        }*/
+
+//if -else exampal.
+let Marks="99";
+if(Marks>=100){
+    console.log("Gread A");
+}else if(Marks>=101){
+    console.log("Gread B");
+}else if(Marks>98){
+    console.log("Gread c");
+}else{
+    console.log("Gread")
+}
+
+
+
