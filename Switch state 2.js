584c3eb7dc0efa37583399e780e7bdd0761a3383
AshwiniGaikwad3819 <ashwinigaikwad3819@gmail.com>
2024-10-22 16:41:38 +0530

    Switch Statement

---
+++ Switch state 2.js
@@ -0,0 +1,16 @@
+let Fruit="Tomato";
+switch (Fruit){
+    case "Apple":
+    case"Banana":
+    case "Mango":
+     console.log("This is a Fruit") ;
+     break;
+     case "potato":
+     case "Tomato":
+     case "Carrot":
+     console.log("This is a Vegetables");
+     break;
+     Default:
+     console.log("Unkown");
+       
+}
