e89b9e586fbaa03bd2bd3023d3f2d452db55f953
AshwiniGaikwad3819 <ashwinigaikwad3819@gmail.com>
2024-10-23 15:00:37 +0530

    Test

---
+++ Test1.js
@@ -0,0 +1,36 @@
+/*1]  let firstName = 'Ashwini';
+let lastname='Gaikwad';
+let batch ='class B';
+
+console.log (firstName);
+console.log(typeof(firstName));*/
+
+
+/*let b1= true;
+let b2=false;
+
+console.log(b1);
+console.log(typeof(b2));*/
+
+
+/*let = null;
+const key=Symbol("12345678");
+let v=88899998989898;
+
+console.log(1);
+console.log(key);
+console.log(v);
+
+console.log(typeof(1));
+console.log(typeof(key));
+console.log(typeof(v));*/
+
+let student={
+    id:1,
+    name:"salman",
+}
+
+console.log(student);
+
+
+
---
+++ Test2.js
@@ -0,0 +1,38 @@
+/*let num=123;
+console.log("A"+num);
+
+let v = "abc"+ 123;
+console.log(v);
+
+let x = true+ 10;
+console.log(typeof(x));
+
+let z ="55" ==55;
+consople.log(z);
+
+let s = "456";
+console.log(typeof(s));
+
+let n= Number(s);
+onsole.log(typeof n);
+
+let g =parsefloat("12.5");
+console.log(typeof g);
+
+let y= 99;
+console.log(typeof y);
+
+let W =String(x);
+console.log(typeof x);*/
+
+
+let a = x.toString();
+console.log(typeof a);
+
+let  b=boolean(0);
+console.log(b);
+
+let m=!!0;
+console.log(m);
+
+
